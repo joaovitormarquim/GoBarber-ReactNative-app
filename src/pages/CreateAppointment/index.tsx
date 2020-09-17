eb96ec4b465309bc19cacf60fba4a16fe863c645
jvmsmfn <jvmsmfn@cin.ufpe.br>
2020-09-17 14:30:52 -0300

    Create Appointment's header

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
 
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
+
 const CreateAppointment: React.FC = () => {
-  const { signOut } = useAuth();
+  const route = useRoute();
+  const { user } = useAuth();
+  const { goBack } = useNavigation();
+  const { providerId } = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  console.log(providerId);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color={'#999591'} />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
   );
 };
 
